6a3b50a2c2c7bf5796ac8a061fb9a9ba8729c314
kusuma009 <kus2263@gmail.com>
2021-07-09 20:37:02 +0530

    login

--- shoppingreact/src/components/login.js
+++ shoppingreact/src/components/login.js
@@ -1,2 +1,11 @@
 
  
+class login extends React.Component{
+    render(){
+             return (<form><h2>Login</h2>
+                           <input type="text" placeholder="enter your Name" /><br/><br/>
+                           <input type="submit" nvalue="log" >submit</input>                
+                      </form>);
+             }
+};
+export default login;
